181eb86bea7218451e5d7be21d0789ba9709e7bb
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-09-22 10:41:47 +0200

    Added function for generating random set of colours

--- Mastermind.cpp
+++ Mastermind.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
 #include <random>
 #include <vector>
 #include <string>
+
+using namespace std;
 
 vector<string> allowed_colours = {"red", "green", "yellow", "blue", "purple", "orange", "white", "black"};
 vector<string> colours_to_guess;
@@ -9,7 +12,10 @@ void set_colours()
 {
     for(int i = 0; i<4; i++)
     {
-        ;
+        // Find the random number in the range [min, max]
+        int rd_colour = rand() % (allowed_colours.size() - 1);
+        colours_to_guess.push_back(allowed_colours.at(rd_colour));
+        cout << colours_to_guess.at(i) << "\n";
     }
 }
 
@@ -19,5 +25,5 @@ using namespace std;
 
 void main()
 {
-    ;
+    set_colours();
 }
