038095184ad48f4390165e20352d87fba19bfa55
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-09-22 10:26:51 +0200

    Include the actual Mastermind-game

---
+++ Mastermind.cpp
@@ -0,0 +1,23 @@
+#include <random>
+#include <vector>
+#include <string>
+
+vector<string> allowed_colours = {"red", "green", "yellow", "blue", "purple", "orange", "white", "black"};
+vector<string> colours_to_guess;
+
+void set_colours()
+{
+    for(int i = 0; i<4; i++)
+    {
+        ;
+    }
+}
+
+using namespace std;
+
+
+
+void main()
+{
+    ;
+}
