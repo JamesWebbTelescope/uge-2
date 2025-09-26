a60fa7501378827feacba7d7ca51cb75bd1330fa
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-09-26 10:33:54 +0200

    Attempted fixing my build

---
+++ out/build/Visual Studio Build Tools 2022 Release - amd64/.qt/QtDeployTargets-Release.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_uge2_FILE C:/Users/SPAC-O-1/Projekter/uge-2/out/build/Visual Studio Build Tools 2022 Release - amd64/Release/uge2.exe)
+set(__QT_DEPLOY_TARGET_uge2_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_uge2_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
