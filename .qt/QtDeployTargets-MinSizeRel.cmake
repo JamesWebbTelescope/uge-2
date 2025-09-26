1c32eb495aae4610201dd4a6ac0b5c7ca942d707
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-09-26 10:18:53 +0200

    Added CMake

---
+++ .qt/QtDeployTargets-MinSizeRel.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_uge2_FILE C:/Users/SPAC-O-1/Projekter/uge-2/MinSizeRel/uge2.exe)
+set(__QT_DEPLOY_TARGET_uge2_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_uge2_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/SPAC-O-1/Projekter/uge-2
+# Build directory: C:/Users/SPAC-O-1/Projekter/uge-2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
